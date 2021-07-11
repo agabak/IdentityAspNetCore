09c9fb071867dddaec41e2fcf6854f8a8d8d355c
Agaba Kabunga <agaba_k@hotmail.com>
2021-07-10 22:08:00 -0500

    Add  view models, controller and view

---
+++ Controllers/Accounts/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using IdentityAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityAspNetCore.Controllers.Accounts
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityAspNetCore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} must be less than {1}")]
+        [Display(Name ="First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} must be less than {1}")]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Must match Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
